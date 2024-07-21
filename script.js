501f6d24cb7090e98b875d9e743748265df859e8
HarshwardhanZalte <harshwardhanzalte9@gmail.com>
2024-07-21 22:12:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+document.getElementById('search-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const query = document.getElementById('search-input').value;
+    searchRecipes(query);
+});
+
+function searchRecipes(query) {
+    const apiKey = '3d6cca8b88db4fdb893a4472e1a84a03';
+    const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&number=10&apiKey=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.results && data.results.length > 0) {
+                displayRecipes(data.results);
+            } else {
+                displayNoResults();
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching the recipes:', error);
+            displayError(error);
+        });
+}
+
+function displayRecipes(recipes) {
+    const recipesContainer = document.getElementById('recipes-container');
+    recipesContainer.innerHTML = '';
+
+    recipes.forEach(recipe => {
+        const recipeElement = document.createElement('div');
+        recipeElement.classList.add('recipe');
+
+        const recipeImage = recipe.image ? `<img src="${recipe.image}" alt="${recipe.title}">` : '';
+
+        recipeElement.innerHTML = `
+            ${recipeImage}
+            <h2>${recipe.title}</h2>
+            <a href="https://spoonacular.com/recipes/${recipe.title}-${recipe.id}" target="_blank">View Recipe</a>
+        `;
+
+        recipesContainer.appendChild(recipeElement);
+        
+    });
+}
+
+function displayNoResults() {
+    const recipesContainer = document.getElementById('recipes-container');
+    recipesContainer.innerHTML = '<p>No recipes found. Please try another search.</p>';
+}
+
+function displayError(error) {
+    const recipesContainer = document.getElementById('recipes-container');
+    recipesContainer.innerHTML = `<p>Error: ${error.message}</p>`;
+}
